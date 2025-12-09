f1d5bf8f604f5a2beb1abd8a5ed4e8b07dc45f6a
014-code <2402143478@qq.com>
2025-12-09 15:07:24 +0800

    初始化rn移动端项目: 1.项目搭建 + 安装rn动画库

---
+++ ai-code-mobile/app/_layout.tsx
@@ -0,0 +1,28 @@
+import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import { Stack } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
+import 'react-native-reanimated';
+
+import { useColorScheme } from '@/hooks/use-color-scheme';
+
+export const unstable_settings = {
+  anchor: '(tabs)',
+};
+
+/**
+ * 通用主题配置
+ * @returns 
+ */
+export default function RootLayout() {
+  const colorScheme = useColorScheme();
+
+  return (
+    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="modal" options={{ presentation: 'modal', title: '模态框' }} />
+      </Stack>
+      <StatusBar style="auto" />
+    </ThemeProvider>
+  );
+}
